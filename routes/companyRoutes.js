03431a192c3b1a6036e44de0085948e7432c48e3
sudarshan sabat <onyourmarkgodigital@gmail.com>
2025-12-30 15:46:18 +0530

    Update companyRoutes.js

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -10,6 +10,14 @@ const { updateCompanyProfile } = require("../controllers/companyController");
 router.post("/register", upload.single("photoUrl"), registerCompany);
 router.post("/login", loginCompany);
 
+// ✅ TEST GET ROUTE
+router.get("/", (req, res) => {
+  res.json({
+    status: "OK",
+    message: "Company API is working 🚀",
+  });
+});
+
 router.put("/profile/:id", updateCompanyProfile);
 
 module.exports = router;

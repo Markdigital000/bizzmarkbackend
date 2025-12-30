58da1c06c3346d6b8be299f41eb7d53ac552dd62
sudarshan sabat <onyourmarkgodigital@gmail.com>
2025-12-30 15:26:31 +0530

    Update app.js1

--- app.js
+++ app.js
@@ -5,18 +5,17 @@ const multer = require("multer");
 const companyRoutes = require("./routes/companyRoutes");
 
 const app = express();
-app.get("/", (req, res) => {
-  res.send("🚀 Bizzmark Backend API is running");
-});
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const upload = multer();
+// ✅ ROOT ROUTE
+app.get("/", (req, res) => {
+  res.send("🚀 Bizzmark Backend API is running");
+});
 
-// Routes
+// API ROUTES
 app.use("/api/companies", companyRoutes);
 
 module.exports = app;

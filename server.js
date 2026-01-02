0e66f69ae11b4b681246cbf4ee9ee68278528cf7
sudarshan sabat <onyourmarkgodigital@gmail.com>
2026-01-02 14:59:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log("ENV CHECK 👉", {
+  PORT: process.env.PORT,
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+});
+
 import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;

2ca3ecc9a79db8ebc439b1ba5bffddff1a61e82a
sudarshan sabat <onyourmarkgodigital@gmail.com>
2026-01-02 14:20:33 +0530

    Update companyRoutes.js

--- routes/companyRoutes.js
+++ routes/companyRoutes.js
@@ -1,27 +1,12 @@
 import express from "express";
-import upload from "../middleware/upload.js";
 import {
-  registerCompany,
-  loginCompany,
-  updateCompanyProfile,
-  getAllCompanies
+  createCompany,
+  getCompanies,
 } from "../controllers/companyController.js";
 
 const router = express.Router();
 
-/* ✅ TEST ROUTE */
-router.get("/", (req, res) => {
-  res.json({
-    status: "OK",
-    message: "Company API is working 🚀"
-  });
-});
-
-/* ✅ GET ALL COMPANIES */
-router.get("/list", getAllCompanies);
-
-router.post("/register", upload.single("photoUrl"), registerCompany);
-router.post("/login", loginCompany);
-router.put("/profile/:id", updateCompanyProfile);
+router.post("/create", createCompany);
+router.get("/list", getCompanies);
 
 export default router;

8d14bb5acacc1f76a2c9f006b7475c7fb63db09a
sudarshan sabat <onyourmarkgodigital@gmail.com>
2026-01-02 16:03:47 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,21 +3,19 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
   ssl: { rejectUnauthorized: false },
 });
 
 pool.on("connect", () => {
-  console.log("✅ PostgreSQL pool connected");
-  console.log("✅ Neon Database connected successfully");
+  console.log("🔗 PostgreSQL pool connected");
 });
 
 pool.on("error", (err) => {
-  console.error("❌ Unexpected DB error:", err);
-  process.exit(1);
+  console.error("❌ PG error", err);
 });
 
 export default pool;
